a94e2da850fe123e8af833e85e6f522a91ab06e7
chanchuan <chanchuan.yeh@gsit.com.tw>
2019-03-19 15:50:13 +0800

    忘記密碼頁

--- src/app/find-password/find-password.component.ts
+++ src/app/find-password/find-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-find-password',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./find-password.component.css']
 })
 export class FindPasswordComponent implements OnInit {
-
-  constructor() { }
+  findPwdForm = new FormGroup({
+    txt_code: new FormControl(''),
+    txt_email: new FormControl(''),
+    chk_mode: new FormControl(''),
+    txt_parentCode: new FormControl(''),
+  })
+  constructor(private router: Router, private location: Location) { }
 
   ngOnInit() {
+  }
+  onSubmit() {
+    let msg = '';
+    if (this.findPwdForm.value.chk_mode) {
+      msg += `已請主管[${this.findPwdForm.value.txt_parentCode}]發送預設密碼到 ${this.findPwdForm.value.txt_email}`
+    } else {
+      msg = `已發送預設密碼到 ${this.findPwdForm.value.txt_email} `
+    }
+    alert(msg);
+    this.gotoPrevious();
+  }
+  gotoPrevious() {
+    this.location.back();
   }
 
 }
