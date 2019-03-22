d525d418a8a293cc86d3208eba6ad1afc8647739
chanchuan <chanchuan.yeh@gsit.com.tw>
2019-03-22 17:59:02 +0800

    增加lib=>model 作為db model 應加app => webapi 抓取db 資料 修改npm start 一次run timecard 和 webapi 起來

---
+++ projects/model/src/lib/model.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-Model',
+  template: `
+    <p>
+      model works!
+    </p>
+  `,
+  styles: []
+})
+export class ModelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/model/src/lib/model.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ModelComponent } from './model.component';
+
+@NgModule({
+  declarations: [ModelComponent],
+  imports: [
+  ],
+  exports: [ModelComponent]
+})
+export class ModelModule { }
